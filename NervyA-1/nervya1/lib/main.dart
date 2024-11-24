ad1b6ba0bb211c3c638befcf292049c6ecbe0fd0
ArelyX1 <andrycaceres2006@gmail.com>
2024-11-24 10:15:49 -0500

    add 3d models

--- NervyA-1/nervya1/lib/main.dart
+++ NervyA-1/nervya1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_3d_controller/flutter_3d_controller.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World! vr'),
+        appBar: AppBar(
+          title: const Text('Flutter 3D'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Damaged Helmet',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                const SizedBox(
+                  height: 400,
+                  child: Flutter3DViewer(
+                    src: 'assets/3d/mono.glb',
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
